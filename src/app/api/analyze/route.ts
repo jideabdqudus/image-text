fa9a85fdc9cee73c0473809ff0baea4242b2c8ab
Jide <jideabdqudus@gmail.com>
2026-02-02 23:48:32 +0000

    fix: update LLM

--- src/app/api/analyze/route.ts
+++ src/app/api/analyze/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: Request) {
       );
     }
     const res = await together.chat.completions.create({
-      model: "meta-llama/Llama-4-Scout-17B-16E-Instruct",
+      model: "meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8",
       messages: [
         {
           role: "system",
