76673dd4e86ec05555e6bd95550e1f315b28692a
Jide <jideabdqudus@gmail.com>
2026-03-24 22:18:03 +0000

    fix: update LLM

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,22 +5,14 @@ export const Footer: React.FC = () => {
     <footer className="container mx-auto py-4 text-center text-sm text-gray-500">
       <p>
         Built by{" "}
-        <a
-          href="https://abdulqudus.com"
-          className="underline"
-          target="_blank"
-        >
+        <a href="https://abdulqudus.com" className="underline" target="_blank">
           Jide
         </a>
         . Powered by{" "}
         <a href="https://together.ai" className="underline" target="_blank">
           Together.ai
         </a>{" "}
-        &{" "}
-        <a href="https://www.llama.com/" className="underline" target="_blank">
-          Llama 4
-        </a>
-        .
+        & Kimi K2.5.
       </p>
     </footer>
   );
